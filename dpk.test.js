5c9dc5e96fb451e502e0bb022e0982bffe2ab559
FreakingCoder <sridhar.affa@gmail.com>
2023-02-13 17:44:11 -0800

    Unit Test coverage for deterministicPartitionKey

--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,23 @@
 const { deterministicPartitionKey } = require("./dpk");
 
 describe("deterministicPartitionKey", () => {
+  
   it("Returns the literal '0' when given no input", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
   });
+  
+  it("When event.partitionKey is null", () => {
+    let event = {partitionKey: null}; 
+    const trivialKey = deterministicPartitionKey(event);
+    expect(trivialKey.length).toBeGreaterThan(0);
+  });
+  
+  
+  it("When event.partitionKey is 123", () => {
+    let event = {partitionKey: 123}; 
+    const trivialKey = deterministicPartitionKey(event);
+    expect(trivialKey).toBe("123");
+  });
+  
 });
